8942e0c7b9c3eedc3f1e2552c01f460964359151
Abhiram B.S.N <abhirambsn@gmail.com>
2022-08-19 21:07:18 +0530

    Update 19-08-2022

--- authors.php
+++ authors.php
@@ -1,12 +1,52 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-12 d-flex justify-content-center">
+        <div class="col-12 d-flex justify-content-center pb-5">
             <span class="section-head text-center fw-bold">
                 Author Guidelines
             </span>
         </div>
-        <div class="col-12 p-5">
-            <p class="tbu text-center">To be updated soon..</p>    
+        <div class="col-sm-12 col-lg-6 px-5">
+            <div class="about-box-1">
+                <h4 class="fw-bold fs-4 d-flex justify-content-center">Call for Papers</h4>
+                <hr />
+                <p class="text-justify">
+                    The broad topics of interest include, but are not limited to, the following:
+                <ul>
+                    <li>Algebra and Its Applications</li>
+                    <li>Analysis and Approximation Theory</li>
+                    <li>Coding, Cryptography and Information Theory</li>
+                    <li>Computational Fluid Dynamics</li>
+                    <li>Computer Graphics & Animation</li>
+                    <li>Continuum Mechanics and Vibrations</li>
+                    <li>Differential Equations and Applications</li>
+                    <li>Discrete Mathematical Structures</li>
+                    <li>Fixed Point Theory</li>
+                    <li>Fractals, Chaos and Dynamical Systems</li>
+                    <li>Fuzzy Mathematics and Logic</li>
+                    <li>Image Processing</li>
+                    <li>Numerical Analysis</li>
+                    <li>Optimization and Its Applications</li>
+                    <li>Probability, Statistics and Stochastic Processes</li>
+                    <li>Theory of Computation</li>
+                    <li>Wave Propagation</li>
+                    <li>Wavelets and Applications</li>
+                </ul>
+                </p>
+            </div>
+        </div>
+        <div class="col-sm-12 col-lg-6 px-5" style="border-left: 1px solid black;">
+            <div class="author-box-1">
+                <h4 class="fw-bold fs-4 d-flex justify-content-center">Important Points</h4>
+                <hr />
+                <p class="text-justify">
+                <ol class="fs-6">
+                    <li>The registration fee for Offline Mode includes conference kit, lunch and tea during the conference</li>
+                    <li>Only accepted and presented papers, after peer review, will be published in Indexed Proceedings/Journals (Proposal to be submitted). The publication charges (if any) for the full length accepted papers will be charged separately.</li>
+                    <li>For On the Spot Registration, 20% extra charges will be paid by the participants</li>
+                    <li>For Early Bird Registration (Till November 02, 2022), 10% discount will be provided to the participants.</li>
+                </ol>
+                </p>
+            </div>
         </div>
     </div>
 </div>
--- committee.php
+++ committee.php
@@ -5,8 +5,43 @@
                 Committee
             </span>
         </div>
-        <div class="col-12 p-5">
-            <p class="tbu text-center">To be updated soon..</p>    
+        <div class="col-6 p-5">
+            <div class="accordion" id="chiefPatron">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading1">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                            Chief Patron
+                        </button>
+                    </h2>
+                    <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1" data-bs-parent="#chiefPatron">
+                        <div class="accordion-body">
+                            <ul>
+                                <li>Shri Jaiprakash Gaur Ji, Founder Chancellor, JIIT</li>
+                                <li>Shri Manoj Gaur Ji, Executive Chairman, Jaypee Group.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 p-5">
+            <div class="accordion" id="patron">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading2">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                            Patron
+                        </button>
+                    </h2>
+                    <div id="collapse2" class="accordion-collapse collapse show" aria-labelledby="heading2" data-bs-parent="#patron">
+                        <div class="accordion-body">
+                            <ul>
+                                <li>Prof. S. C. Saxena, Pro Chancellor, JIIT</li>
+                                <li>Prof. Y. R. Sood, Vice Chancellor, JIIT</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
