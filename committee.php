739795b23540d4d11a18760acc283cb2fa0793a1
Abhiram B.S.N <abhirambsn@gmail.com>
2022-08-19 21:40:02 +0530

    Update 19-08-2022

--- committee.php
+++ committee.php
@@ -5,15 +5,15 @@
                 Committee
             </span>
         </div>
-        <div class="col-6 p-5">
+        <div class="col-lg-6 col-xs-12 mb-4 mt-2 px-5">
             <div class="accordion" id="chiefPatron">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="heading1">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
                             Chief Patron
                         </button>
                     </h2>
-                    <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="heading1" data-bs-parent="#chiefPatron">
+                    <div id="collapse1" class="accordion-collapse collapse" aria-labelledby="heading1" data-bs-parent="#chiefPatron">
                         <div class="accordion-body">
                             <ul>
                                 <li>Shri Jaiprakash Gaur Ji, Founder Chancellor, JIIT</li>
@@ -24,20 +24,75 @@
                 </div>
             </div>
         </div>
-        <div class="col-6 p-5">
+        <div class="col-lg-6 col-xs-12 mb-4 mt-2 px-5">
             <div class="accordion" id="patron">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="heading2">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
                             Patron
                         </button>
                     </h2>
-                    <div id="collapse2" class="accordion-collapse collapse show" aria-labelledby="heading2" data-bs-parent="#patron">
+                    <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading2" data-bs-parent="#patron">
                         <div class="accordion-body">
                             <ul>
                                 <li>Prof. S. C. Saxena, Pro Chancellor, JIIT</li>
                                 <li>Prof. Y. R. Sood, Vice Chancellor, JIIT</li>
                             </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 px-5">
+            <div class="accordion" id="teamMem">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading3">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                            Institutional Advisory Committee
+                        </button>
+                    </h2>
+                    <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading3" data-bs-parent="#teamMem">
+                        <div class="accordion-body">
+                            <ul id="instAdvCom">
+                                <li>Prof. B. R. Mehta, Director, DRID, Sector 128, JIIT</li>
+                                <li>Prof. D.K. Rai, Dean (A&R), JIIT</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading4">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="true" aria-controls="collapse4">
+                            Departmental Advisory Committee
+                        </button>
+                    </h2>
+                    <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading4" data-bs-parent="#teamMem">
+                        <div class="accordion-body">
+                            <ul id="deptAdvCom"></ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading5">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="true" aria-controls="collapse5">
+                            International/National Advisory Committee
+                        </button>
+                    </h2>
+                    <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading5" data-bs-parent="#teamMem">
+                        <div class="accordion-body">
+                            <ul id="intlAdvCom"></ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="heading6">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse6" aria-expanded="true" aria-controls="collapse6">
+                            List of Invited Speakers
+                        </button>
+                    </h2>
+                    <div id="collapse6" class="accordion-collapse collapse" aria-labelledby="heading6" data-bs-parent="#teamMem">
+                        <div class="accordion-body">
+                            <ul id="speakers"></ul>
                         </div>
                     </div>
                 </div>
