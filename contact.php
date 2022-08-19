ad13b4de4159de2e6e06e6bec846226bdf0eadbb
Ayush Bansal <ayushb6732@gmail.com>
2022-08-19 21:59:28 +0530

    Contact updated

--- contact.php
+++ contact.php
@@ -12,27 +12,32 @@
             <div class="d-flex flex-column">
                 <h6 class="fw-bold fs-4">Jaypee Institute of Information Technology</h6>
                 <div class="px-4 fw-bold">
-                <span class="fs-6">A-10, A - Block, Industrial Area, Sector - 62</span> <br>
-                <span class="fs-6">Noida, Uttar Pradesh, India - 201309</span>
+                    <span class="fs-6">A-10, A - Block, Industrial Area, Sector - 62</span> <br>
+                    <span class="fs-6">Noida, Uttar Pradesh, India - 201309</span>
                 </div>
                 <hr>
                 <div class="d-flex flex-column">
-                    <h5 class="fs-4 fw-bold">How to reach</h5>
-
+                    <h5 class="fs-5 fw-bold">Address your communications to</h5>
+                    <br>
                     <div class="px-4">
-                    <h6 class="fw-bold">Metro:</h6>
-                    <p>The nearest metro station is NOIDA ELECTRONIC CITY. <br/>The institute is situated at a walking distance from the<br/> metro station.</p>
+                        <p>
+                            <b>Prof. Alka Tripathi</b>, (Conference Chair) <br>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Head, Department of Mathematics <br>
+                            <b>Dr. Yogesh Gupta, Dr. Lakhveer Kaur</b>, (Conveners) <br>
+                            <hr>
+                            <b>Mobile: +91 9953891505, +91 9899273989 <br>
+                            Email: <a href="mailto:ramsaconference.jiit@gmail.com">ramsaconference.jiit@gmail.com</a> </b>
 
-                    <h6 class="fw-bold">Railway:</h6>
-                    <p>Noida is well connected to the New Delhi, Old Delhi and <br/>Ghaziabad railway stations by bus. Cabs can be hired just outside <br/>the railway stations</p>
 
-                    <h6 class="fw-bold">Airport:</h6>
-                    <p>Cabs can be hired just outside the airport terminal for JIIT Sector‐62, Noida.</p>
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="d-flex flex-column">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.0011016336703!2d77.36986691490839!3d28.629729082418958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce551491b3ce7%3A0x7335d9fcfd4d9db0!2sJAYPEE%20INSTITUTE%20OF%20INFORMATION%20TECHNOLOGY!5e0!3m2!1sen!2sin!4v1651330408389!5m2!1sen!2sin" class="frame" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.0011016336703!2d77.36986691490839!3d28.629729082418958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce551491b3ce7%3A0x7335d9fcfd4d9db0!2sJAYPEE%20INSTITUTE%20OF%20INFORMATION%20TECHNOLOGY!5e0!3m2!1sen!2sin!4v1651330408389!5m2!1sen!2sin"
+                    class="frame" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
