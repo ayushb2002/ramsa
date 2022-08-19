aaac4f414f403daba617d8eeeadafc187aee5ab5
Abhiram B.S.N <abhirambsn@gmail.com>
2022-08-19 11:12:38 +0530

    Contact us done

--- contact.php
+++ contact.php
@@ -5,8 +5,31 @@
                 Contact Us
             </span>
         </div>
-        <div class="col-12 p-5">
+        <!-- <div class="col-12 p-5">
             <p class="tbu text-center">To be updated soon..</p>    
+        </div> -->
+        <div class="d-flex flex-lg-row flex-column align-items-start justify-content-evenly py-3 mx-4">
+            <div class="d-flex flex-column">
+                <h6 class="fw-bold fs-3">Jaypee Institute of Information Technology</h6>
+                <span class="fs-5">A-10, A - Block, Industrial Area,</span>
+                <span class="fs-5">Sector - 62, Noida, U.P.</span>
+
+                <div class="d-flex flex-column my-2">
+                    <h5 class="fs-4 fw-bold">How to reach</h5>
+
+                    <h6 class="fw-bold">Metro:</h6>
+                    <p>The nearest metro station is NOIDA ELECTRONIC CITY. <br/>The institute is situated at a walking distance from the<br/> metro station.</p>
+
+                    <h6 class="fw-bold">Railway:</h6>
+                    <p>Noida is well connected to the New Delhi, Old Delhi and <br/>Ghaziabad railway stations by bus. Cabs can be hired just outside <br/>the railway stations</p>
+
+                    <h6 class="fw-bold">Airport:</h6>
+                    <p>Cabs can be hired just outside the airport terminal for JIIT Sector‐62, Noida.</p>
+                </div>
+            </div>
+            <div class="d-flex flex-column">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.0011016336703!2d77.36986691490839!3d28.629729082418958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce551491b3ce7%3A0x7335d9fcfd4d9db0!2sJAYPEE%20INSTITUTE%20OF%20INFORMATION%20TECHNOLOGY!5e0!3m2!1sen!2sin!4v1651330408389!5m2!1sen!2sin" class="frame" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
     </div>
 </div>
