5b47dac7f19d6f237ad57dda666fdbeb33ac2a7e
Shikha Pandey <sp1486@gmail.com>
2022-10-18 19:13:46 +0530

    18Oct

--- news.php
+++ news.php
@@ -7,7 +7,7 @@
             <ul>
             <li><span>The abstract submission link is out now! Submit </span><a href="https://forms.gle/4XJarSyH6FTq7WHi9" target="_blank">here</a></li>
                 <li><span>Form for abstract submission is out now. Head to <a href="#abstract">Author Guidelines</a> section to download the template for submission.</span></li>
-                <li><span>The registration link is out now! Apply </span><a href="https://forms.gle/BXWC29y5oXSJLW1v7" target="_blank">here</a></li>
+                <li><span>The registration link is out now! Apply </span><a href="https://forms.gle/95zuXjs5vs2A5eFj9" target="_blank">here</a></li>
                 <li><a href="resources/RAMSA-2022.pdf" target="_blank">RAMSA-2022 Brochure.</a></li>
                 <li><span class="multi-color">Full paper submission link is out now! Submit </span><a href="https://easychair.org/my/conference?conf=ramsa2022" target="_blank">here</a></li>               
             </ul>
@@ -28,15 +28,11 @@
                 </tr>
                 <tr>
                     <td>Submission of Full Length Paper</td>
-                    <td><strike>October 05, 2022</strike> <br>October 15, 2022</td>
+                    <td><strike>October 15, 2022</strike> <br>October 30, 2022</td>
                 </tr>
                 <tr>
                     <td>Notification of Acceptance of Paper</td>
-                    <td>October 25, 2022</td>
-                </tr>
-                <tr>
-                    <td>Submission of Final Manuscript</td>
-                    <td>November 5, 2022</td>
+                    <td>November 15, 2022</td>
                 </tr>
             </table>
         </div>
